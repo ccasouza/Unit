bd41ec6ee5484d40aa607e827547a38bbd4ef67a
Clérison Souza <60799279+ccasouza@users.noreply.github.com>
2020-09-05 23:35:52 -0300

    Add files via upload

---
+++ src/Lista2/Transporte.java
@@ -0,0 +1,82 @@
+package Lista2;
+/*Professor Tito, Samuel e eu fizemos juntos. Tanto ele quanto eu, 
+  não estávamos conseguindo evoluir individualmente.
+  Nos unimos, então, para conseguir fazer o mínimo.
+  O senhor verificará que existem muitos erros, mas foi até onde conseguimos ir.
+  A avaliação prática, infelizmente, não conseguimos evoluir.
+ */
+
+
+public class Transporte {
+
+	// Atributos
+	private Integer id;
+	private Integer ano;
+	private String modelo;
+	private double carga_maxima;
+	private double potencia;
+
+	// Construtor padrão
+	public Transporte() {
+
+	}
+
+	// Construtor com assinatura
+	public Transporte(Integer id, Integer ano, String modelo, double carga_maxima, double portencia) {
+
+	}
+
+	// Métodos get e set
+	public int getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public double getCarga_maxima() {
+		return carga_maxima;
+	}
+
+	public void setCarga_maxima(double carga_maxima) {
+		this.carga_maxima = carga_maxima;
+	}
+
+	public double getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(double potencia) {
+		this.potencia = potencia;
+	}
+
+	// Metodos Públicos
+	public double consumo() {
+		return ((potencia * carga_maxima) * 100);
+	}
+
+	public String leiaModelo() {
+		return this.leiaModelo();
+	}
+
+	public void gravaModelo(String modelo) {
+		return;
+	}
+}
